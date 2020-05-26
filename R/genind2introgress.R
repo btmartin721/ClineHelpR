27008c0269810318d4d167ede80ccc50f033914c
Tyler Chafin <tkchafin@uark.edu>
2020-05-26 16:27:01 -0500

    added metadata section for genind2introgress

--- R/genind2introgress.R
+++ R/genind2introgress.R
@@ -1,31 +1,33 @@
 #!/usr/local/bin/R
-#' Aggregate BGC Output
+#'Generate inputs for Introgress
 #'
-#' This function aggregates multiple BGC runs into one file. Runs are joined
-#' by columns and must all contain the same number of post-burnin MCMC
-#' iterations. It assumes that input files are contained in a single directory
-#' and have specific file suffixes. See the README.md file for details on
-#' file suffixes. Files can have population prefixes.
+#' This function parses a genind object to create input data frames for 
+#' running genomic cline analysis in the Introgress R package
 #'
-#' @param results.dir Path to a directory containing all the BGC results
-#' @param prefix Prefix to the input files; e.g., "population1"
-#' @param thin Thin to every n MCMC samples. E.g. thin=2 cuts samples in half
-#' @param discard Discard first N samples from each BGC run
-#' @return A list of data.frames (each data.frame is a BGC parameter)
+#' @param gen Genind object containing data for parental and admixed populations
+#' @param p1 Character or vector containing population names for the p1 group
+#' @param p2 Character or vector containing population names for the p2 group
+#' @param admix Character or vector containing population names for the admixed individuals
+#' @param missingPerInd Proportion of missing genotypes allowed to retain an individual 
+#' @param missingPerLoc Proportion of missing genotypes allowed to retain a locus
+#' @param subset An optional parameter defining a number of loci to randomly retain
+#' @param drop Boolean value defining whether or not to remove monomorphic loci
+#' @return A list of data.frames (each data.frame is an introgress input)
 #' @export
 #' @examples
-#' aggregate.results <- combine_bgc_output(results.dir = "./results",
-#'                                         prefix = "population1")
+#' introgress_input <- genind2introgress(genind, p1=c("PopA", "PopB"),
+#'                                         p2="PopC", admix=c("PopD1", "PopD2"))
 #'
-#' aggregate.results <- combine_bgc_output(results.dir = "./results",
-#'                                         prefix = "population2",
-#'                                         thin = 2)
+#' introgress_input <- genind2introgress(genind, p1=c("PopA", "PopB"),
+#'                                         p2="PopC", admix=c("PopD1", "PopD2"),
+#'                                         missingPerInd=0.75, missingPerLoc=0.25,
+#'                                         drop=TRUE)
 #'
-#' aggregate.results <- combine_bgc_output(results.dir = "./results",
-#'                                         prefix = "pop3",
-#'                                         discard = 2000)
+#' introgress_input <- genind2introgress(genind, p1=c("PopA", "PopB"),
+#'                                         p2="PopC", admix=c("PopD1", "PopD2"),
+#'                                         subset=500)
 genind2introgress <- function(
-	genind,
+	gen,
 	p1,
 	p2,
 	admix,
@@ -40,7 +42,7 @@ genind2introgress <- function(
 	all_pops<-c(p1, p2, admix)
 	
 	#drop individuals not in selected pops
-	sub<-genind[pop(genind) %in% all_pops,]
+	sub<-gen[pop(gen) %in% all_pops,]
 	
 	#drop loci having more than missingPerLoc missing data
 	sub<-filter_missingByLoc(sub, prop=missingPerLoc)
@@ -56,43 +58,44 @@ genind2introgress <- function(
 	
 	#begin parsing remaining data to create inputs for introgress 
 	#set up loci information table ()
-	loci.data<-data.frame(cbind(c(names(genind$loc.n.all)), genind$type), stringsAsFactors = FALSE)
+	loci.data<-data.frame(cbind(c(names(sub$loc.n.all)), sub$type), stringsAsFactors = FALSE)
 	colnames(loci.data)<-c("locus", "type")
 	loci.data[loci.data["type"]=="codom", "type"]<-"C"
 	loci.data[loci.data["type"]=="dom", "type"]<-"D"
 	
-	#genotype tables for p1
+	#genotype table for p1
 	p1_genind<-sub[pop(sub) %in% p1,]
 	p1.data<-adegenet::genind2df(p1_genind, sep="/")
 	p1.data<-subset(p1.data, select=-c(pop))
 	p1.data[is.na(p1.data)] <- "NA/NA"
 	
-	#genotype tables for p2
+	#genotype table for p2
 	p2_genind<-sub[pop(sub) %in% p2,]
 	p2.data<-adegenet::genind2df(p2_genind, sep="/")
 	p2.data<-subset(p2.data, select=-c(pop))
 	p2.data[is.na(p2.data)] <- "NA/NA"
 	
+	#genotype table for admix population
 }
 
 ###############################################
 #Utility functions
-filter_missingByLoc<-function(genind, prop=0.5){
-	missing<-propTyped(genind, by="loc")
+filter_missingByLoc<-function(gen, prop=0.5){
+	missing<-propTyped(gen, by="loc")
 	#print(genind[loc=c(missing>prop)])
-	return(genind[loc=c(missing>prop)])
+	return(gen[loc=c(missing>prop)])
 }
 
-filter_missingByInd<-function(genind, prop=0.5, drop=TRUE){
-	missing<-propTyped(genind, by="ind")
+filter_missingByInd<-function(gen, prop=0.5, drop=TRUE){
+	missing<-propTyped(gen, by="ind")
 	#print(missing>prop)
 	return(genind[c(missing>prop), drop=drop])
 }
 
-filter_randomSubsetLoci<-function(genind, sample){
-	if (sample < nLoc(genind)){
-		locs <- sort(sample(1:nLoc(genind), sample, replace=FALSE))
-		return(genind[loc=c(locs)])
+filter_randomSubsetLoci<-function(gen, sample){
+	if (sample < nLoc(gen)){
+		locs <- sort(sample(1:nLoc(gen), sample, replace=FALSE))
+		return(gen[loc=c(locs)])
 	}
 }
 
