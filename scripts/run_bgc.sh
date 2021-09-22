e49d3ef2fae8b77c858c2d7c067492f580640a9c
Bradley Martin <evobio721@gmail.com>
2021-09-22 10:38:49 -0500

    Added docker integration and improved run_bgc.sh

--- scripts/run_bgc.sh
+++ scripts/run_bgc.sh
@@ -1,15 +1,58 @@
 #!/bin/bash
 
+################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+   # Display Help
+   echo "Run bgc and estpost."
+   echo
+   echo "Usage: run_bgc.sh -s <PATH_TO_BGC_SETTINGS_FILE>"
+   echo
+   echo "Options:"
+   echo "-h     Print this help menu."
+   echo "-s     Path to BGC settinigs file."
+   echo
+}
+
+# Get the options
+while getopts "hs:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      s) # BGC settings file
+	 BGC_SETTINGS=${OPTARG}
+	 ;;
+     \?) # incorrect option
+	 echo "Error: Invalid option"
+	 Help
+	 exit;;
+   esac
+done
+
 # ------------------------------------------------------------------------------
 # --- Run bgc
 # ------------------------------------------------------------------------------
 
-mcmc=$3
-burnin=$4
-thin=$5
-gt_uncert=$6
-error=$7
-linkage=$8
+if [ -z "$BGC_SETTINGS" ]; then
+        echo "You must supply the path to the bgc_settings.txt file"
+        echo "Usage: run_bgc.sh -s <PATH_TO_BGC_SETTINGS_FILE>"
+	Help
+        exit 1;
+fi
+
+if [ ! -f "$BGC_SETTINGS" ]; then
+        echo "Couldn't find specified BGC settings file!"
+	echo "Usage: run_bgc.sh -s <PATH_TO_BGC_SETTINGS_FILE>"
+	Help
+        exit 1;
+fi
+
+# Source bgc settings into environment from file
+source $BGC_SETTINGS
+export $(sed '/^#/d' $BGC_SETTINGS | cut -d= -f1)
 
 # Arguments set
 # -O (0, 1, or 2)     : Format to write MCMC samples (default=0)
@@ -19,7 +62,7 @@ linkage=$8
 # -x (integer)    : Number of MCMC generations
 # -n (integer)    : discard n steps as burn-in
 # -t (integer)    : sample MCMC chain every t generations
-# -N (0 or 1)     : Whether to use genotype uncertainty model (recommended) (default=0)
+# -N (0 or 1)     : Whether to use genotype uncertainty model (recommended for NGS data) (default=0)
 # -E (float)      : Use sequence error model and genotype uncertainty model (default=0)
 #                 :     Error rate can be specified with a float (e.g. 0.001 = .1% error rate)
 # -q (0 or 1)     : Whether to calculate cline parameter quantiles (default=0)
@@ -42,17 +85,71 @@ linkage=$8
 # -e (float)      : standard deviation for Gaussian proposal of cline parameters eta and kappa (default=0.02)
 # -D (float)      : Maximum distance between loci, free recombination. Only used for linkage model (default=0.5)
 
+p0in=${INPUT_DATA_DIR}/${prefix}_p0in.txt
+p1in=${INPUT_DATA_DIR}/${prefix}_p1in.txt
+admixedin=${INPUT_DATA_DIR}/${prefix}_admixedin.txt
+
+if [ ! -f "$p0in" ]; then
+       	echo "Could not find parent0 input file!"
+	echo "Check the prefix in the bgc settings file,"
+	echo " make sure it is in the location you specified in the bgc_settings file,"
+	echo " and make sure it has the filename suffix *_p0in.txt"
+	exit 2;
+fi
+
+if [ ! -f "$p1in" ]; then
+	echo "Could not find parent1 input file!"
+        echo "Check the prefix in the bgc settings file,"
+        echo " make sure it is in the location you specified in the bgc_settings file,"
+        echo " and make sure it has the filename suffix *_p1in.txt"
+        exit 2;
+fi
+
+if [ ! -f "$admixedin" ]; then
+	echo "Could not find admixed input file!"
+        echo "Check the prefix in the bgc settings file,"
+        echo "make sure it is in the location you specified in the bgc_settings file,"
+        echo " and make sure it has the filename suffix *_admixedin.txt"
+        exit 2;
+fi
+
 # Not sure how BGC manages seeding. So in case it's random by clock time, I staggered them here.
 sleepytime=`shuf -i 10-240 -n 1`
 sleep "$sleepytime"
 
-if [ $linkage = 0 ]; then
-	$HOME/local/bin/bgc -a data/${2}_bgc_p0in.txt -b data/${2}_bgc_p1in.txt -h data/${2}_bgc_admixedin.txt -O 0 -x $mcmc -n $burnin -t $thin -N $gt_uncert -E $error -m $linkage -q 1 -I 1 -p 1 -F results/${2}_bgc_mcmcout_${1};
+echo "Running BGC. Results will be located in the ${RESULTS_DIR}/ directory..."
+
+if [ $linkage -eq 0 ]; then
+	${BGC_PATH}/bgc -a ${INPUT_DATA_DIR}/${prefix}_p0in.txt \
+		-b ${INPUT_DATA_DIR}/${prefix}_p1in.txt \
+		-h ${INPUT_DATA_DIR}/${prefix}_admixedin.txt \
+		-O 0 -x $mcmc -n $burnin -t $thin -N $gt_uncert \
+		-E $error -m $linkage -q 1 -I 1 -p 1 \
+		-F ${RESULTS_DIR}/${prefix}_mcmcout_${run} \
+		-g $stddev_gamma -z $stddev_zeta \
+		-e $stddev_eta_kappa -u $mcmc_tuning;
 
 else
-	$HOME/local/bin/bgc -a data/${2}_bgc_p0in.txt -b data/${2}_bgc_p1in.txt -h data/${2}_bgc_admixedin.txt -O 0 -x $mcmc -n $burnin -t $thin -N $gt_uncert -E $error -m $linkage -M data/${2}_bgc_map.txt -q 1 -I 1 -p 1 -F results/${2}_bgc_mcmcout_${1};
-
+	linkagefile=${INPUT_DATA_DIR}/${prefix}_map.txt
+	if [ ! -f "$linkagefile" ]; then
+		echo "Could not find linkage map input file!"
+        	echo "Check the prefix in the bgc settings file,"
+        	echo " make sure it is in the location you specified in the bgc_settings file,"
+        	echo " and make sure it has the filename suffix *_bgc_map.txt"
+        	exit 2;
+	fi
+	${BGC_PATH}/bgc -a ${INPUT_DATA_DIR}/${prefix}_p0in.txt \
+		-b ${INPUT_DATA_DIR}/${prefix}_p1in.txt \
+		-h ${INPUT_DATA_DIR}/${prefix}_admixedin.txt \
+		-O 0 -x $mcmc -n $burnin -t $thin -N $gt_uncert \
+		-E $error -m $linkage -M ${INPUT_DATA_DIR}/${prefix}_map.txt \
+		-D $linkage_dist -q 1 -I 1 -p 1 \
+		-F ${RESULTS_DIR}/${prefix}_mcmcout_${run} \
+		-g $stddev_gamma -z $stddev_zeta \
+		-e $stddev_eta_kappa -u $mcmc_tuning;
 fi
+
+echo "Done running BGC!"
 
 # ------------------------------------------------------------------------------
 # --- Run estpost
@@ -76,23 +173,33 @@ fi
 # -b (integer)     : Discard b generations as burn-in (based on number of thinned samples) (default=0)
 # -h (integer)     : Number of bins for posterior sample histogram (default=20)
 
+echo "Running estpost; Results will be located in the ${RESULTS_DIR}/ directory"
+
 # Get log-likelihood
-$HOME/local/bin/estpost -i results/${2}_bgc_mcmcout_${1}.hdf5 -p LnL -o results/${2}_bgc_stat_ln1_${1} -s 2 -w 0
+${ESTPOST_PATH}/estpost -i ${RESULTS_DIR}/${prefix}_mcmcout_${run}.hdf5 \
+	-p LnL -o ${RESULTS_DIR}/${prefix}_stat_ln1_${run} -s 2 -w 0
 
 # Get alpha
-$HOME/local/bin/estpost -i results/${2}_bgc_mcmcout_${1}.hdf5 -p alpha -o results/${2}_bgc_stat_a0_${1} -s 2 -w 0
+${ESTPOST_PATH}/estpost -i ${RESULTS_DIR}/${prefix}_mcmcout_${run}.hdf5 \
+	-p alpha -o ${RESULTS_DIR}/${prefix}_stat_a0_${run} -s 2 -w 0
 
 # Get beta
-$HOME/local/bin/estpost -i results/${2}_bgc_mcmcout_${1}.hdf5 -p beta -o results/${2}_bgc_stat_b0_${1} -s 2 -w 0
+${ESTPOST_PATH}/estpost -i ${RESULTS_DIR}/${prefix}_mcmcout_${run}.hdf5 \
+	-p beta -o ${RESULTS_DIR}/${prefix}_stat_b0_${run} -s 2 -w 0
 
 # Get hybrid index
-$HOME/local/bin/estpost -i results/${2}_bgc_mcmcout_${1}.hdf5 -p hi -o results/${2}_bgc_stat_hi_${1} -s 2 -w 0
+${ESTPOST_PATH}/estpost -i ${RESULTS_DIR}/${prefix}_mcmcout_${run}.hdf5 \
+	-p hi -o ${RESULTS_DIR}/${prefix}_stat_hi_${run} -s 2 -w 0
 
 # Get gamma-quantile (qa)
-$HOME/local/bin/estpost -i results/${2}_bgc_mcmcout_${1}.hdf5 -p gamma-quantile -o results/${2}_bgc_stat_qa_${1} -s 2 -w 0
+$ESTPOST_PATH/estpost -i ${RESULTS_DIR}/${prefix}_mcmcout_${run}.hdf5 \
+	-p gamma-quantile -o ${RESULTS_DIR}/${prefix}_stat_qa_${run} -s 2 -w 0
 
 # Get zeta-quantile (qb)
-$HOME/local/bin/estpost -i results/${2}_bgc_mcmcout_${1}.hdf5 -p zeta-quantile -o results/${2}_bgc_stat_qb_${1} -s 2 -w 0
+$ESTPOST_PATH/estpost -i ${RESULTS_DIR}/${prefix}_mcmcout_${run}.hdf5 \
+	-p zeta-quantile -o ${RESULTS_DIR}/${prefix}_stat_qb_${run} -s 2 -w 0
 
-exit
+echo "Done with estpost!"
+
+exit 0
 
